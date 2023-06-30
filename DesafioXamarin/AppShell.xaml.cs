d93eecbe3ed10193ba9e43a87257a74fd499e623
Higor <higor.pereira@mobrj.com.br>
2023-06-30 17:42:59 -0300

    Projeto Xamarin adicionado

---
+++ DesafioXamarin/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using DesafioXamarin.ViewModels;
+using DesafioXamarin.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DesafioXamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ DesafioXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DesafioXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DesafioXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
