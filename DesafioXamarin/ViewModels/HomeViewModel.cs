423f03238442f7cf22c9438ebfb8283d3deecfa0
Higor <higor.pereira@mobrj.com.br>
2023-06-30 21:12:54 -0300

    Limpando a HomeViewModel

--- DesafioXamarin/ViewModels/HomeViewModel.cs
+++ DesafioXamarin/ViewModels/HomeViewModel.cs
@@ -10,9 +10,6 @@ namespace DesafioXamarin.ViewModels
         public HomeViewModel()
         {
             Title = "Home";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
-
-        public ICommand OpenWebCommand { get; }
     }
 }
